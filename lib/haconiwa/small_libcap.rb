c0444ef4f5af5ab8eb0425b4ebd13cdcc6cbe5ec
Uchio Kondo <udzura@udzura.jp>
2016-06-01 16:33:14 +0900

    Implement whitelist logic

--- lib/haconiwa/small_libcap.rb
+++ lib/haconiwa/small_libcap.rb
@@ -28,18 +28,35 @@ module Haconiwa
       cap_get_bound(cap) >= 0
     end
 
-    def self.drop_cap_by_name(name)
+    def self._name2cap(name)
       ptr = FFI::MemoryPointer.new(:int)
       err = cap_from_name(name, ptr)
       if err < 0
         raise CapError, "Invalid or unsupported capability name: #{name}"
       end
+      ptr.read_int
+    end
 
-      err = cap_drop_bound(ptr.read_int)
+    def self.drop_cap_by_name(name)
+      err = cap_drop_bound(_name2cap(name))
       if err < 0
         raise CapError, "Failed to drop capability name: #{name} from bounding set"
       end
       true
+    end
+
+    def self.apply_cap_whitelist(list: [])
+      whitelist = list.map{|n| _name2cap(n) }
+
+      loop.with_index(0) do |_, cap_value|
+        return(true) unless cap_supported?(cap_value)
+        next if whitelist.include?(cap_value)
+
+        err = cap_drop_bound(cap_value)
+        if err < 0
+          raise CapError, "Failed to drop capability cap_value_t: #{cap_value} from bounding set"
+        end
+      end
     end
   end
 end
